8a8b2d85c6343ef64f11838235d99b309d71c02d
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-10-06 03:36:10 -0700

    Prints all the values on the stack, starting from the top of the stack

--- pall.c
+++ pall.c
@@ -1,20 +1,13 @@
 #include "monty.h"
-/**
-* op_pall - prints out the stack
-* @stack: the stack
-* @line_number: the line
-*/
-void op_pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
 
-	(void)line_number;
-	if (!*stack)
-		return;
-	temp = *stack;
-	while (temp != NULL)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	print_stack(*stack);
 }
