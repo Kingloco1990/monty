2471eef39852647014c88d2b6d3e4a7be60c4fef
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-10-06 03:35:16 -0700

    Pushes an element to the stack

--- push.c
+++ push.c
@@ -1,30 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
-/**
- * op_push - pushes integers to the stack or queue still working on edge cases
- * @stack: the memory
- * @line_number: the line
- */
-void op_push(stack_t **stack, unsigned int line_number)
-{
-	stack_t *fresh;
 
-	if (!stack)
-		exit(EXIT_FAILURE);
-	if (!(isnum(argument_container.arguments)))
+/**
+ * push - push element into the stack
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_cnt)
+{
+	char *n = global.argument;
+
+	if ((is_digit(n)) == 0)
 	{
-		printf("L%u: usage: push integer\n", line_number);
-		free_stack(stack);
+		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	fresh = malloc(sizeof(stack_t));
-	if (fresh == NULL)
+
+	if (global.data_struct == 1)
 	{
-		printf("Error: malloc failed\n");
-		free_stack(stack);
-		exit(EXIT_FAILURE);
+		if (!add_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
 	}
-	if (argument_container.stack_queue == 0)
-		nasus(stack, fresh);
 	else
-		veigar(stack, fresh);
+	{
+		if (!queue_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
 }
