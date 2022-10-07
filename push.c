caf0cb5e81f1c8c38350480dda559617b3ac262f
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-10-07 12:03:38 -0700

    Pushes an element to the stack

--- push.c
+++ push.c
@@ -1,38 +1,64 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * push - push element into the stack
- * @stack: stack given by main
- * @line_cnt: amount of lines
- *
- * Return: void
- */
-void push(stack_t **stack, unsigned int line_cnt)
+  * push - Adds a new node at the beginning of the stack
+  * @stack: The head of the stack
+  * @param: The value to adds on the stack
+  *
+  * Return: Nothing
+  */
+void push(stack_t **stack, unsigned int param)
 {
-	char *n = global.argument;
+	stack_t *new_node = NULL;
 
-	if ((is_digit(n)) == 0)
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-		exit(EXIT_FAILURE);
+		new_node->next = *stack;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+		return;
 	}
 
-	if (global.data_struct == 1)
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
+}
+
+/**
+  * push_queue - Adds a new node at the end of the stack
+  * @stack: The head of the stack
+  * @param: The value to adds on the stack
+  *
+  * Return: Nothing
+  */
+void push_queue(stack_t **stack, unsigned int param)
+{
+	stack_t *current = NULL, *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
 	{
-		if (!add_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		current = *stack;
+		while (current->next != NULL)
+			current = current->next;
+
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return;
 	}
-	else
-	{
-		if (!queue_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
-	}
+
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
 }
